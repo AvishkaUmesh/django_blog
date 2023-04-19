5290618e8fe52568f2c8df757b5da30548d080d1
Avishka Umesh <umesh.avishka@gmail.com>
2023-04-19 17:10:32 +0530

    updated templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.start_page, name="blog-home"),
-    path("/posts", views.posts, name="blog-posts"),
-    path("/post/<slug:slug>", views.post_details, name="blog-post-details"),
+    path("posts", views.posts, name="blog-posts"),
+    path("post/<slug:slug>", views.post_details, name="blog-post-details"),
 ]
