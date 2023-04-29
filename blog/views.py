ba02d841e779e02b9a677989a2009dac5e933593
Avishka Umesh <umesh.avishka@gmail.com>
2023-04-29 21:40:26 +0530

    added comments for posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from typing import Any, Dict
-from django.db.models.query import QuerySet
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views import View
+from django.urls import reverse
+
+from blog.forms import CommentForm
 from .models import Post
 
 
@@ -43,12 +46,31 @@ def post_details(request, slug):
     )
 
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-details.html"
-    model = Post
-    context_object_name = "post"
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),  # type: ignore
+        }
+        return render(request, "blog/post-details.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()  # type: ignore
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = get_object_or_404(Post, slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("blog-post-details", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),  # type: ignore
+        }
+        return render(request, "blog/post-details.html", context)
